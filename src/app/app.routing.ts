e924dfa01ce328222b477c42e084445e06cdac44
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-06-27 23:06:48 -0700

    Add beauty routing, not working yet

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BeautyComponent } from './beauty/beauty.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'beauty',
+    component: BeautyComponent
   }
 ];
 
