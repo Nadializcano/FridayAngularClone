512fd6393e273bee7f2dacfa017f9969571f3c07
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-06-28 14:29:35 -0700

    Add properties to beauty class

--- src/app/models/beauty.model.ts
+++ src/app/models/beauty.model.ts
@@ -1,5 +1,9 @@
 export class Beauty {
-  constructor(public title: string)
+  constructor(public title: string,
+    public category: string,
+    public article: string,
+    public image: string,
+    public id: number)
   {
 
   }
